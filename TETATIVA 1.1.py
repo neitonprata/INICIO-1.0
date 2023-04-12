975bfa415c45be1361ed34f787c2977cfead9988
nnnn nnnnn <prataneiton@gmail.com>
2023-04-12 19:47:38 -0300

    esse é a tentativa de enviar para branch tetativa1, para depois enviar para a branch principal main

--- TETATIVA 1.1.py
+++ TETATIVA 1.1.py
@@ -1,1 +1,3 @@
 print ("agora consegui trocar para branch de develop, ou seja a branch de teste, para depois enviar para a main")
+print ("essa é a segunda mensagem que edito ")
+print ("nesse momento vou inserir essa edição na branch tetnativa 1 para depois jogar para mais")
